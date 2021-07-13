3f72b4698bdae960f659a0e0ebba40c5aba41a3d
Mirsagatov <62383145+Mirsagatov@users.noreply.github.com>
2021-07-13 12:36:37 +0500

    done

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var elForm = document.querySelector('.form');
+var elNumberInput = elForm.querySelector('.form-input');
+var elSelectInput = elForm.querySelector('.form-select')
+var elResultBox = elForm.querySelector('.result-box');
+
+
+
+//console.log(fizzBuzz(30));
+
+function submitElForm(evt) {
+	evt.preventDefault();
+	var numberInput = Number(elNumberInput.value.trim());
+	elResultBox.textContent = `${numberInput}UZS = ${numberInput/elSelectInput.value}`;
+}
+
+elForm.addEventListener('submit', submitElForm);
